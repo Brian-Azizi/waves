78ce7f117a7f57f9ba39f0ca3fa8af466868ca9f
Brian Azizi <briana@theodo.co.uk>
2019-12-26 16:00:19 +0100

    Create window

--- src/com/brian/main/Game.java
+++ src/com/brian/main/Game.java
@@ -1,6 +1,18 @@
 package com.brian.main;
 
-public class Game {
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+    private static final long serialVersionUID = -8993431008488886099L;
+
+    public synchronized void start() {
+
+    }
+
+    public void run() {
+
+    }
+
     public static void main(String[] args) {
 
     }
--- src/com/brian/main/Window.java
+++ src/com/brian/main/Window.java
@@ -1,7 +1,25 @@
 package com.brian.main;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
 
 public class Window extends Canvas {
     private static final long serialVersionUID = 7222038341159421793L;
+
+    public Window(int width, int height, String title, Game game) {
+        JFrame frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.add(game);
+        frame.setVisible(true);
+        game.start();
+    }
 }
