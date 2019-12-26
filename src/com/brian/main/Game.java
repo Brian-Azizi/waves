625bf4880da8b641063d7dcc883af80e271fe6a5
Brian Azizi <briana@theodo.co.uk>
2019-12-26 16:25:36 +0100

    add stop method

--- src/com/brian/main/Game.java
+++ src/com/brian/main/Game.java
@@ -4,14 +4,29 @@ import java.awt.Canvas;
 
 public class Game extends Canvas implements Runnable {
     private static final long serialVersionUID = -8993431008488886099L;
+
     public final static int WIDTH = 640, HEIGHT = WIDTH * 9 / 12;
+
+    private Thread thread;
+    private boolean running = false;
 
     public Game() {
         new Window(WIDTH, HEIGHT, "Waves", this);
     }
 
     public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
 
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run() {
