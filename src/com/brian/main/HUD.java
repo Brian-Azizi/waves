b6cbabb53183ea309e084740181723c6cd7d160c
Brian Azizi <briana@theodo.co.uk>
2019-12-28 01:40:41 +0100

    Make healthbar go red on low health

--- src/com/brian/main/HUD.java
+++ src/com/brian/main/HUD.java
@@ -14,7 +14,7 @@ public class HUD {
     public void render(Graphics g) {
         g.setColor(Color.gray);
         g.fillRect(15, 15, 200, 32);
-        g.setColor(Color.green);
+        g.setColor(Color.getHSBColor((1f * HEALTH) / 360, 1f, 1f));
         g.fillRect(15, 15, HEALTH * 2, 32);
         g.setColor(Color.white);
         g.drawRect(15, 15, 200, 32);
