51b6b8e39e693d43c9065c65294df95d32c71ba5
Brian Azizi <briana@theodo.co.uk>
2019-12-28 12:14:29 +0100

    Fix sticky keys

--- src/com/brian/main/KeyInput.java
+++ src/com/brian/main/KeyInput.java
@@ -5,6 +5,7 @@ import java.awt.event.KeyEvent;
 
 public class KeyInput extends KeyAdapter {
     private Handler handler;
+    private boolean[] keyDown = { false, false, false, false };
 
     public KeyInput(Handler handler) {
         this.handler = handler;
@@ -21,15 +22,19 @@ public class KeyInput extends KeyAdapter {
 
                 if (key == KeyEvent.VK_W) {
                     tempObject.setVelY(-5);
+                    keyDown[0] = true;
                 }
                 if (key == KeyEvent.VK_S) {
                     tempObject.setVelY(5);
-                }
-                if (key == KeyEvent.VK_A) {
-                    tempObject.setVelX(-5);
+                    keyDown[1] = true;
                 }
                 if (key == KeyEvent.VK_D) {
                     tempObject.setVelX(5);
+                    keyDown[2] = true;
+                }
+                if (key == KeyEvent.VK_A) {
+                    tempObject.setVelX(-5);
+                    keyDown[3] = true;
                 }
             }
         }
@@ -47,18 +52,20 @@ public class KeyInput extends KeyAdapter {
             if (tempObject.getId() == ID.Player) {
                 // Key events for plater
 
-                if (key == KeyEvent.VK_W) {
+                if (key == KeyEvent.VK_W)
+                    keyDown[0] = false;
+                if (key == KeyEvent.VK_S)
+                    keyDown[1] = false;
+                if (key == KeyEvent.VK_D)
+                    keyDown[2] = false;
+                if (key == KeyEvent.VK_A)
+                    keyDown[3] = false;
+
+                if (!keyDown[0] && !keyDown[1])
                     tempObject.setVelY(0);
-                }
-                if (key == KeyEvent.VK_S) {
-                    tempObject.setVelY(0);
-                }
-                if (key == KeyEvent.VK_A) {
+                if (!keyDown[2] && !keyDown[3])
                     tempObject.setVelX(0);
-                }
-                if (key == KeyEvent.VK_D) {
-                    tempObject.setVelX(0);
-                }
+
             }
         }
     }
